4af9ce2861cdf0a736f52e59c697458efddc17f0
Mayur Deshmukh <mayur.s.deshmukh10@gmail.com>
2020-09-07 12:55:22 +0530

    initial setup

---
+++ db/pg.go
@@ -0,0 +1,134 @@
+package db
+
+import (
+	"database/sql"
+	"errors"
+	"fmt"
+	"joshsoftware/go-e-commerce/config"
+	"os"
+	"strconv"
+	"time"
+
+	"github.com/jmoiron/sqlx"
+	_ "github.com/lib/pq"
+	"github.com/mattes/migrate"
+	"github.com/mattes/migrate/database/postgres"
+	_ "github.com/mattes/migrate/source/file"
+	logger "github.com/sirupsen/logrus"
+)
+
+const (
+	dbDriver      = "postgres"
+	migrationPath = "./migrations"
+)
+
+var errFindingDriver = errors.New("no migrate driver instance found")
+
+type pgStore struct {
+	db *sqlx.DB
+}
+
+func Init() (s Storer, err error) {
+	uri := config.ReadEnvString("DB_URI")
+
+	conn, err := sqlx.Connect(dbDriver, uri)
+	if err != nil {
+		logger.WithField("err", err.Error()).Error("Cannot initialize database")
+		return
+	}
+
+	logger.WithField("uri", uri).Info("Connected to pg database")
+	return &pgStore{conn}, nil
+}
+
+func RunMigrations() (err error) {
+	uri := config.ReadEnvString("DB_URI")
+
+	db, err := sql.Open(dbDriver, uri)
+	if err != nil {
+		return
+	}
+
+	driver, err := postgres.WithInstance(db, &postgres.Config{})
+	if err != nil {
+		return
+	}
+
+	m, err := migrate.NewWithDatabaseInstance(getMigrationPath(), dbDriver, driver)
+	if err != nil {
+		return
+	}
+
+	err = m.Up()
+	if err == migrate.ErrNoChange || err == nil {
+		err = nil
+		return
+	}
+
+	return
+}
+
+func CreateMigrationFile(filename string) (err error) {
+	if len(filename) == 0 {
+		err = errors.New("filename is not provided")
+		return
+	}
+
+	timeStamp := time.Now().Unix()
+	upMigrationFilePath := fmt.Sprintf("%s/%d_%s.up.sql", migrationPath, timeStamp, filename)
+	downMigrationFilePath := fmt.Sprintf("%s/%d_%s.down.sql", migrationPath, timeStamp, filename)
+
+	err = createFile(upMigrationFilePath)
+	if err != nil {
+		return
+	}
+
+	err = createFile(downMigrationFilePath)
+	if err != nil {
+		os.Remove(upMigrationFilePath)
+		return
+	}
+
+	logger.WithFields(logger.Fields{
+		"up":   upMigrationFilePath,
+		"down": downMigrationFilePath,
+	}).Info("Created migration files")
+
+	return
+}
+
+func RollbackMigrations(s string) (err error) {
+	uri := config.ReadEnvString("DB_URI")
+
+	steps, err := strconv.Atoi(s)
+	if err != nil {
+		return
+	}
+
+	m, err := migrate.New(getMigrationPath(), uri)
+	if err != nil {
+		return
+	}
+
+	err = m.Steps(-1 * steps)
+	if err == migrate.ErrNoChange || err == nil {
+		err = nil
+		return
+	}
+
+	return
+}
+
+func createFile(filename string) (err error) {
+	f, err := os.Create(filename)
+	if err != nil {
+		return
+	}
+
+	err = f.Close()
+	return
+}
+
+func getMigrationPath() string {
+	return fmt.Sprintf("file://%s", migrationPath)
+}
