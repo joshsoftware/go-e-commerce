39734b45c7822cc077acf0044eb9808544d88944
sagar23sj <sagarsonwane23@gmail.com>
2020-09-08 03:29:48 +0530

    Called GetUserByMobile in Storer Interface

--- db/db.go
+++ db/db.go
@@ -4,8 +4,10 @@ import (
 	"context"
 )
 
+// Storer - an interface we use to expose methods that do stuff to the underlying database
 type Storer interface {
 	ListUsers(context.Context) ([]User, error)
+	GetUserByMobile(context.Context, string) (User, error)
 	//Create(context.Context, User) error
 	//GetUser(context.Context) (User, error)
 	//Delete(context.Context, string) error
