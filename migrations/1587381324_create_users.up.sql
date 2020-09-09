340650c39f2ace0892494a6aadfc01b75c322376
sagar23sj <sagarsonwane23@gmail.com>
2020-09-09 20:40:06 +0530

    Removed NOT NULL constraint from Mobile from User

--- migrations/1587381324_create_users.up.sql
+++ migrations/1587381324_create_users.up.sql
@@ -3,7 +3,7 @@ CREATE TABLE IF NOT EXISTS users (
   first_name VARCHAR(255) NOT NULL,
   last_name VARCHAR(255) NOT NULL,
   email VARCHAR(255) NOT NULL UNIQUE,
-  mobile VARCHAR(20) NOT NULL UNIQUE,
+  mobile VARCHAR(20) UNIQUE,
   country VARCHAR(100),
   state VARCHAR(100),
   city VARCHAR(100),
