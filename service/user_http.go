dcbaa0176eef937d8e78e3b21bf782928f827b55
sagar23sj <sagarsonwane23@gmail.com>
2020-09-09 10:57:28 +0530

    Added getUserHandler

--- service/user_http.go
+++ service/user_http.go
@@ -2,9 +2,10 @@ package service
 
 import (
 	"encoding/json"
-	"net/http"
-
+	"github.com/gorilla/mux"
 	logger "github.com/sirupsen/logrus"
+	"net/http"
+	"strconv"
 )
 
 // @Title listUsers
@@ -23,6 +24,38 @@ func listUsersHandler(deps Dependencies) http.HandlerFunc {
 		}
 
 		respBytes, err := json.Marshal(users)
+		if err != nil {
+			logger.WithField("err", err.Error()).Error("Error marshaling users data")
+			rw.WriteHeader(http.StatusInternalServerError)
+			return
+		}
+
+		rw.Header().Add("Content-Type", "application/json")
+		rw.Write(respBytes)
+	})
+}
+
+// @Title getUser
+// @Description Show User User
+// @Router /user/{id} [get]
+// @Accept  json
+// @Success 200 {object}
+// @Failure 400 {object}
+func getUserHandler(deps Dependencies) http.HandlerFunc {
+	return http.HandlerFunc(func(rw http.ResponseWriter, req *http.Request) {
+		var idParam = mux.Vars(req)["id"]
+		id, err := strconv.Atoi(idParam)
+		if err != nil {
+			logger.Error("COnversion Failed")
+		}
+		user, err := deps.Store.GetUser(req.Context(), id)
+		if err != nil {
+			logger.WithField("err", err.Error()).Error("Error fetching data")
+			rw.WriteHeader(http.StatusInternalServerError)
+			return
+		}
+
+		respBytes, err := json.Marshal(user)
 		if err != nil {
 			logger.WithField("err", err.Error()).Error("Error marshaling users data")
 			rw.WriteHeader(http.StatusInternalServerError)
