43de8673ded9116badb68198c011ee5ebe6a1fb2
Rohit Nale <rohitnale738@gmail.com>
2020-09-11 12:43:38 +0530

    add insert migration file

---
+++ migration.sql
@@ -0,0 +1,58 @@
+INSERT INTO category (name, description) VALUES ('Clothes','All wearable fabrics, ');
+INSERT INTO category (name, description) VALUES('Electronics',' stores or generates electricity');
+INSERT INTO category (name, description) VALUES('Mobile','The mobile phone can be used to communicate ');
+INSERT INTO category (name, description) VALUES('Watch','A watch is a portable timepiece intended ');
+INSERT INTO category (name, description) VALUES('Books','There are several things to consider in order');
+INSERT INTO category (name, description) VALUES('Sports','Shoes are for regular comfort wear');
+
+
+
+INSERT INTO products (name, description, price, discount, quantity, category_id) VALUES('Polo Shirt', 'Benetton Men  Classic Fit Polo Shirt',511, 10, 5, 1 );
+INSERT INTO products (name, description, price, discount, quantity, category_id) VALUES('Wrangler', 'Men  Slim Fit Jeans', 600, 20, 5, 1 );
+INSERT INTO products (name, description, price, discount, quantity, category_id) VALUES('Dragon Jacket','Made from the skin of one of the dragons', 700, 40, 5, 1 );
+INSERT INTO products (name, description, price, discount, quantity, category_id) VALUES('HD Ready Android LED TV ', 'Resolution: HD Ready Android TV (1366x768)', 1200, 20, 10, 2 );
+INSERT INTO products (name, description, price, discount, quantity, category_id) VALUES('Sony DSC W830 Cyber-Shot 20.1 MP ', 'Shoot Camera (Black) with 8X ', 1500, 50, 15, 2 );
+INSERT INTO products (name, description, price, discount, quantity, category_id) VALUES('Charger', 'Mi 10W Charger with Cable (1.2 Meter, Black)', 500, 5, 0, 2 );
+INSERT INTO products (name, description, price, discount, quantity, category_id) VALUES('Apple iPhone 11 Pro (64GB)', '5.8-inch (14.7 cm) ', 60000, 5, 10, 3 );
+INSERT INTO products (name, description, price, discount, quantity, category_id) VALUES('Apple iPhone 11 Pro (64GB) Max', '5.8-inch (14.7 cm) ', 700000, 4, 30, 3 );
+INSERT INTO products (name, description, price, discount, quantity, category_id) VALUES('Apple iPhone XR (64GB)', '6.1-inch (15.5 cm) Liquid Retina HD LCD display', 50000, 6, 20, 3 );
+INSERT INTO products (name, description, price, discount, quantity, category_id) VALUES ('Relax Watch','by wearing it you are bound to feel realaxed',2100, 10, 10, 4 );
+INSERT INTO products (name, description, price, discount, quantity, category_id) VALUES ('Titan Watch','by wearing it you are bound to feel realaxed',2000, 15, 8, 4 );
+INSERT INTO products (name, description, price, discount, quantity, category_id) VALUES ('SonataWatch','by wearing it you are bound to feel realaxed',3010, 20, 12, 4 );
+INSERT INTO products (name, description, price, discount, quantity, category_id) VALUES ('Wings of Fire','autobiography by visionary scientist',332, 10, 5, 5 );
+INSERT INTO products (name, description, price, discount, quantity, category_id) VALUES ('Thoughts to Inspire','famous quotes by Swami Vivekananda', 150, 15, 10, 5 );
+INSERT INTO products (name, description, price, discount, quantity, category_id) VALUES ('Lancer','Mens Running Shoes', 150, 15, 10, 6 );
+INSERT INTO products (name, description, price, discount, quantity, category_id) VALUES ('Football','Sporting Goods', 200, 40, 15, 6 );
+
+
+
+
+
+INSERT INTO productimages VALUES(1,'jacket1.jpg' );
+INSERT INTO productimages VALUES(1,'jacket2.jpg');
+INSERT INTO productimages VALUES(1,'jacket3.jpg');
+INSERT INTO productimages VALUES(1,'jacket1.jpg');
+INSERT INTO productimages VALUES(1,'poloshirt1.jpg');
+
+INSERT INTO productimages VALUES(2,'camera.jpg');
+INSERT INTO productimages VALUES(2,'Tv.jpg');
+INSERT INTO productimages VALUES(2,'charger.jpg');
+
+
+INSERT INTO productimages VALUES(3,'vivo.jpg');
+INSERT INTO productimages VALUES(3,'apple.jpg');
+INSERT INTO productimages VALUES(3,'mi.jpg');
+
+
+INSERT INTO productimages VALUES(4,'watch1.jpg');
+INSERT INTO productimages VALUES(4,'watch2.jpg');
+INSERT INTO productimages VALUES(4,'watch3.jpg');
+
+
+INSERT INTO productimages VALUES(5,'book1.jpg');
+INSERT INTO productimages VALUES(5,'book2.jpg');
+
+
+INSERT INTO productimages VALUES(6,'sport1.jpg');
+INSERT INTO productimages VALUES(6,'sport2.jpg');
+INSERT INTO productimages VALUES(6,'sport3.jpg');
