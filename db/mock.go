6d36484cb47e38b0f9c3b4715040bf19a7a9bcd4
paramita majumdar <parimajumdar2012@gmail.com>
2020-09-15 18:17:22 +0530

    added test cases for User getByID and update

--- db/mock.go
+++ db/mock.go
@@ -10,7 +10,20 @@ type DBMockStore struct {
 	mock.Mock
 }
 
+//ListUsers mock method
 func (m *DBMockStore) ListUsers(ctx context.Context) (users []User, err error) {
 	args := m.Called(ctx)
 	return args.Get(0).([]User), args.Error(1)
+}
+
+//GetUser mock method
+func (m *DBMockStore) GetUser(ctx context.Context, id int) (user User, err error) {
+	args := m.Called(ctx)
+	return args.Get(0).(User), args.Error(1)
+}
+
+//UpdateUser mock method
+func (m *DBMockStore) UpdateUserByID(ctx context.Context, user User, id int) (err error) {
+	args := m.Called(ctx)
+	return args.Error(0)
 }
--- service/user_http_test.go
+++ service/user_http_test.go
@@ -1,13 +1,15 @@
 package service
 
 import (
+	"encoding/json"
 	"errors"
 	"joshsoftware/go-e-commerce/db"
+	"log"
 	"net/http"
 	"net/http/httptest"
 	"strings"
-	"testing"
 
+	"github.com/bxcodec/faker/v3"
 	"github.com/gorilla/mux"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/mock"
@@ -26,27 +28,32 @@ func (suite *UsersHandlerTestSuite) SetupTest() {
 	suite.dbMock = &db.DBMockStore{}
 }
 
-func TestExampleTestSuite(t *testing.T) {
-	suite.Run(t, new(UsersHandlerTestSuite))
-}
-
 func (suite *UsersHandlerTestSuite) TestListUsersSuccess() {
-	suite.dbMock.On("ListUsers", mock.Anything).Return(
-		[]db.User{
-			db.User{Name: "test-user", Age: 18},
-		},
-		nil,
-	)
+	fakeUser := db.User{}
+	faker.FakeData(&fakeUser)
+
+	// Declare an array of db.User and append the fakeUser onto it for use on the dbMock
+	fakeUsers := []db.User{}
+	fakeUsers = append(fakeUsers, fakeUser)
+
+	suite.dbMock.On("ListUsers", mock.Anything).Return(fakeUsers, nil)
 
 	recorder := makeHTTPCall(
 		http.MethodGet,
+		"/users",
 		"/users",
 		"",
 		listUsersHandler(Dependencies{Store: suite.dbMock}),
 	)
 
+	var users []db.User
+	err := json.Unmarshal(recorder.Body.Bytes(), &users)
+	if err != nil {
+		log.Fatal("Error converting HTTP body from listUsersHandler into User object in json.Unmarshal")
+	}
+
 	assert.Equal(suite.T(), http.StatusOK, recorder.Code)
-	assert.Equal(suite.T(), `[{"full_name":"test-user","age":18}]`, recorder.Body.String())
+	assert.NotNil(suite.T(), users[0].ID)
 	suite.dbMock.AssertExpectations(suite.T())
 }
 
@@ -59,6 +66,7 @@ func (suite *UsersHandlerTestSuite) TestListUsersWhenDBFailure() {
 	recorder := makeHTTPCall(
 		http.MethodGet,
 		"/users",
+		"/users",
 		"",
 		listUsersHandler(Dependencies{Store: suite.dbMock}),
 	)
@@ -67,7 +75,139 @@ func (suite *UsersHandlerTestSuite) TestListUsersWhenDBFailure() {
 	suite.dbMock.AssertExpectations(suite.T())
 }
 
-func makeHTTPCall(method, path, body string, handlerFunc http.HandlerFunc) (recorder *httptest.ResponseRecorder) {
+func (suite *UsersHandlerTestSuite) TestGetUserSuccess() {
+
+	userID := 1
+	suite.dbMock.On("GetUser", mock.Anything, userID).Return(
+		db.User{
+			ID:        1,
+			FirstName: "TestUser",
+			LastName:  "TestUser",
+			Email:     "TestEmail",
+			Mobile:    "TestMobile",
+			Address:   "Testaddress",
+			Password:  "TestPass",
+			Country:   "TestCountry",
+			State:     "TestState",
+			City:      "TestCity",
+		}, nil,
+	)
+
+	recorder := makeHTTPCall(http.MethodGet,
+		"/user",
+		"/user",
+		"",
+		getUserHandler(Dependencies{Store: suite.dbMock}),
+	)
+
+	assert.Equal(suite.T(), http.StatusOK, recorder.Code)
+	assert.Equal(suite.T(), `{ "data": {
+        "id": 1,
+        "first_name": "TestUser",
+        "last_name": "TestUser",
+        "email": "TestEmail",
+		"mobile": "TestMobile",
+		"address": "Testaddress",
+        "password": "TestPass",
+        "country": "TestCountry",
+        "state": "TestState",
+        "city": "TestCity"
+       
+    }}`, recorder.Body.String())
+
+	suite.dbMock.AssertExpectations(suite.T())
+}
+
+func (suite *UsersHandlerTestSuite) TestUpdateUserSuccess() {
+	userID := 1
+	user := db.User{
+		ID:        1,
+		FirstName: "UpdatedUserName",
+		LastName:  "TestUser",
+		Email:     "TestEmail",
+		Mobile:    "TestMobile",
+		Password:  "TestPass",
+		Country:   "TestCountry",
+		State:     "TestState",
+		City:      "TestCity",
+		Address:   "Testaddress",
+	}
+
+	suite.dbMock.On("UpdateUserByID", mock.Anything, user, userID).Return(nil)
+
+	body := ` "id": 1,
+	"first_name": "UpdatedUserName",
+	"last_name": "TestUser",
+	"mobile_number": "TestMobile",
+	"password": "TestPass",
+	"country": "TestCountry",
+	"state": "TestState",
+	"city": "TestCity",
+	"address": "Testaddress"`
+
+	recorder := makeHTTPCall(http.MethodPatch,
+		"/user/update",
+		"/user/update",
+		body,
+		updateUserHandler(Dependencies{Store: suite.dbMock}),
+	)
+
+	assert.Equal(suite.T(), http.StatusOK, recorder.Code)
+	assert.Equal(suite.T(), `{"data": {
+        "id": 1,
+        "first_name": "UpdatedUserName",
+        "last_name": "TestUser",
+        "email": "TestEmail",
+		"mobile": "TestMobile",
+		"address": "Testaddress",
+        "password": "TestPass",
+        "country": "TestCountry",
+        "state": "TestState",
+		"city": "TestCity"
+        
+    }}`, recorder.Body.String())
+	suite.dbMock.AssertExpectations(suite.T())
+}
+
+func (suite *UsersHandlerTestSuite) TestUpdateUserDbFailure() {
+	userID := 1
+	user := db.User{
+		ID:        1,
+		FirstName: "UpdatedUserName",
+		LastName:  "TestUser",
+		Email:     "TestEmail",
+		Mobile:    "TestMobile",
+		Password:  "TestPass",
+		Country:   "TestCountry",
+		State:     "TestState",
+		City:      "TestCity",
+		Address:   "Testaddress",
+	}
+	suite.dbMock.On("UpdateUserByID", mock.Anything, user, userID).Return(errors.New("Error while updating user"))
+
+	body := ` "id": 1,
+	"first_name": "UpdatedUser",
+	"last_name": "TestUser",
+	"mobile": "TestMobile",
+	"address": "Testaddress",
+	"password": "TestPass",
+	"country": "TestCountry",
+	"state": "TestState",
+	"city": "TestCity"
+	`
+
+	recorder := makeHTTPCall(http.MethodPut,
+		"/user/update",
+		"/user/update",
+		body,
+		updateUserHandler(Dependencies{Store: suite.dbMock}),
+	)
+
+	assert.Equal(suite.T(), http.StatusInternalServerError, recorder.Code)
+	suite.dbMock.AssertExpectations(suite.T())
+}
+
+func makeHTTPCall(method, path, requestURL, body string, handlerFunc http.HandlerFunc) (recorder *httptest.ResponseRecorder) {
 	// create a http request using the given parameters
 	req, _ := http.NewRequest(method, path, strings.NewReader(body))
 
