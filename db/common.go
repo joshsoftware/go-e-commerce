d9c220c6894ad6d59400a1f5a6ac0d2daa11d7cb
santoshkavhar <santosh.kavhar@joshsoftware.com>
2020-09-17 21:37:16 +0530

    Removed Product Images Table. Storing ProductImages as an Array in Products table itself. UNIQUE constraints added.

--- db/common.go
+++ db/common.go
@@ -1,6 +1,5 @@
 package db
 
-// ErrorResponse - a struct representing a response for an error
 type ErrorResponse struct {
 	Code    string            `json:"code"`
 	Message string            `json:"message"`
--- migrations/1599715344_productimages.up.sql
+++
@@ -1,6 +0,0 @@
-CREATE TABLE IF NOT EXISTS productimages (
-	product_id int NOT NULL,
-	url text NOT NULL,
-	FOREIGN KEY(product_id) 
-	REFERENCES products(id) ON DELETE CASCADE ON UPDATE CASCADE
-);
