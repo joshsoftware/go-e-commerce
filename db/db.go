f75c1356155d7d7e261d85dd9895b7d1a391802f
Mayur Deshmukh <mayur.s.deshmukh10@gmail.com>
2020-09-17 11:18:21 +0530

    Removed commented code

--- db/db.go
+++ db/db.go
@@ -7,9 +7,5 @@ import (
 type Storer interface {
 	ListUsers(context.Context) ([]User, error)
 	CreateUser(context.Context, User) (User, error)
-	// CheckUserByEmail(context.Context, string) (bool, User, error)
 	GetUserByEmail(context.Context, string) (User, error)
-	//Create(context.Context, User) error
-	//GetUser(context.Context) (User, error)
-	//Delete(context.Context, string) error
 }
